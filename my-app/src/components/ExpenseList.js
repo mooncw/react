ad51f098f231fcb400484fc17e0ef1f4af0e1055
hjlee <hjlee@tkm-t.co.kr>
2024-12-17 09:20:34 +0900

    first commit

---
+++ my-app/src/components/ExpenseList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './ExpenseList.css'
+import ExpenseItem from './ExpenseItem'
+import { MdDelete } from 'react-icons/md'
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <>
+        <ul className='list'>
+          <ExpenseItem />
+        </ul>
+        <button className='btn'>
+          목록 지우기
+          <MdDelete className='btn-icon' />
+        </button>
+      </>
+    )
+  }
+}
+
+export default ExpenseList
